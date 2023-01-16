db7d5626c172fff178a2d0ca4af9322bbf0cb411
Boyd Lever <boydlever@stanfordhacker.com>
2023-01-15 21:19:19 -0800

    Initial Commit

---
+++ src/components/Frame.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+class Frame extends Component{
+    render(){
+        return(
+            <div className="frame">
+                {this.props.children}
+            </div>
+        )
+    }
+}
+export default Frame;
